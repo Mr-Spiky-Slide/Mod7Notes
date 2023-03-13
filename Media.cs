f87e157c4349c0970c5e6ce00a9da98083795306
JackHoward <jhoward000@live.com>
2023-03-13 13:45:34 -0500

    1.12 Adds Runtime and Director

--- Media.cs
+++ Media.cs
@@ -29,27 +29,5 @@ public class Movie : Media
     }
 }
 
-// Album class is derived from Media class
-public class Album : Media
-{
-    public string artist { get; set; }
-    public string recordLabel { get; set; }
 
-    public override string Display()
-    {
-        return $"Id: {mediaId}\nTitle: {title}\nArtist: {artist}\nLabel: {recordLabel}\nGenres: {string.Join(", ", genres)}\n";
-    }
-}
 
-// Book class is derived from Media class
-public class Book : Media
-{
-    public string author { get; set; }
-    public UInt16 pageCount { get; set; }
-    public string publisher { get; set; }
-
-    public override string Display()
-    {
-        return $"Id: {mediaId}\nTitle: {title}\nAuthor: {author}\nPages: {pageCount}\nPublisher: {publisher}\nGenres: {string.Join(", ", genres)}\n";
-    }
-}
--- Program.cs
+++ Program.cs
@@ -5,10 +5,72 @@ string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 // create instance of Logger
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
-logger.Info("Program started");
+logger.Info("Program sxtarted");
 
 string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
 logger.Info(scrubbedFile);
 MovieFile movieFile = new MovieFile(scrubbedFile);
+
+string choice = "";
+do
+{
+  // display choices to user
+  Console.WriteLine("1) Add Movie");
+  Console.WriteLine("2) Display All Movies");
+  Console.WriteLine("Enter to quit");
+  // input selection
+  choice = Console.ReadLine();
+  logger.Info("User choice: {Choice}", choice);
+
+  if (choice == "1")
+  {
+    // Add movie
+    Movie movie = new Movie();
+    // ask user to input movie title
+    Console.WriteLine("Enter movie title");
+    // input title
+    movie.title = Console.ReadLine();
+    // verify title is unique
+    if (movieFile.isUniqueTitle(movie.title)){
+      //input director
+      Console.WriteLine("Enter movie director");
+      movie.director = Console.ReadLine();
+
+      //input runtime
+      Console.WriteLine("Enter running time (h:m:s)");
+      movie.runningTime = TimeSpan.Parse(Console.ReadLine());
+
+      // input genres
+      string input;
+      do
+      {
+        // ask user to enter genre
+        Console.WriteLine("Enter genre (or done to quit)");
+        // input genre
+        input = Console.ReadLine();
+        // if user enters "done"
+        // or does not enter a genre do not add it to list
+        if (input != "done" && input.Length > 0)
+        {
+          movie.genres.Add(input);
+        }
+      } while (input != "done");
+      // specify if no genres are entered
+      if (movie.genres.Count == 0)
+      {
+        movie.genres.Add("(no genres listed)");
+      }
+      // add movie
+      movieFile.AddMovie(movie);
+    }
+  } else if (choice == "2")
+  {
+    // Display All Movies
+    foreach(Movie m in movieFile.Movies)
+    {
+      Console.WriteLine(m.Display());
+    }
+  }
+} while (choice == "1" || choice == "2");
 
 logger.Info("Program ended");
