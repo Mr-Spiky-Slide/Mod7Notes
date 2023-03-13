81172c54aac4648c8d11ff3be080558152b2b646
JackHoward <jhoward000@live.com>
2023-03-13 13:03:55 -0500

    1.06 Add albumn class and instance

--- Media.cs
+++ Media.cs
@@ -24,6 +24,19 @@ public class Media
     public string director { get; set; }
     public TimeSpan runningTime { get; set; }
 
-    
+
+ }
+
+
+ // Album class is derived from Media class
+ public class Album : Media
+ {
+     public string artist { get; set; }
+     public string recordLabel { get; set; }
+
+     public override string Display()
+     {
+         return $"Id: {mediaId}\nTitle: {title}\nArtist: {artist}\nLabel: {recordLabel}\nGenres: {string.Join(", ", genres)}\n";
+     }
  }
 
